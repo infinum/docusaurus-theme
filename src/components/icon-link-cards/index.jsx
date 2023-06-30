bd04384aa38019d1a614f74d76496235187118a7
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-06-30 17:37:38 +0200

    add option to include spacing for open-source project icons

--- src/components/icon-link-cards/index.jsx
+++ src/components/icon-link-cards/index.jsx
@@ -10,12 +10,14 @@ import React from 'react';
  * @param {string} props.cards[].text Card title.
  * @param {string} props.cards[].url Card URL.
  * @param {boolean} [props.gray=false] If passed, the block is set on a light gray background.
+ * @param {boolean} [props.osProjectIcons=false] If passed, the icon spacing is slightly tweaked to match open-source project icons.
  */
 export const IconLinkCards = (props) => {
 	const {
 		title,
 		cards,
 		gray = false,
+		osProjectIcons = false,
 	} = props;
 
 	return (
@@ -26,7 +28,7 @@ export const IconLinkCards = (props) => {
 				{cards.map(({ icon, text, url }, i) => {
 					return (
 						<a className='hover:no-underline hover:text-infinum' href={url} key={i}>
-							{React.cloneElement(icon, {className: 'w-24 h-24 -mx-5 text-infinum'})}
+							{React.cloneElement(icon, {className: `text-infinum ${osProjectIcons ? 'w-20 h-20 -mx-2' : 'w-24 h-24 -mx-5'}`})}
 							<h4 className='text-24 font-bold'>{text}</h4>
 						</a>
 					);
